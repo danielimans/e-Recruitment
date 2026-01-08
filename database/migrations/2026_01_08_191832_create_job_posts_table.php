9afcb8be257b2b6a7e3d2c2355b4da3187109734
Shadow <danieliman1179@gmail.com>
2026-01-09 03:32:56 +0800

    updated

--- database/migrations/2026_01_08_191832_create_job_posts_table.php
+++ database/migrations/2026_01_08_191832_create_job_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('job_posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('location');
             $table->timestamps();
         });
     }
